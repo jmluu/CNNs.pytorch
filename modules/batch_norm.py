555fa67ca29a1c5f3b100c41fabac1b440940796
“lumonk” <lujinming_556@163.com>
2020-03-24 17:02:54 +0800

    add diy bn using autograd.Function

--- modules/batch_norm.py
+++ modules/batch_norm.py
@@ -1,9 +1,9 @@
 # DIY batch-normalization layer 
 import torch 
 import torch.nn as nn 
+from torch.autograd.function import Function
 
-
-class BatchNorm(nn.Module):
+class BatchNorm_v1(nn.Module):
 
     def __init__(self, num_features, eps=1e-5, momentum=0.01, affine=True,
                  track_running_stats=True):
@@ -85,3 +85,92 @@ class BatchNorm(nn.Module):
     def extra_repr(self):
         return '{num_features}, eps={eps}, momentum={momentum}, affine={affine}, ' \
                 'track_running_stata={track_running_stats}'.format(**self.__dict__)
+
+
+class BatchNormFunction(Function):
+    @staticmethod
+    def forward(ctx, input, running_mean, 
+                running_var, 
+                weight, bias,
+                training,
+                affine, 
+                momentum,
+                eps):
+
+        if input.dim() == 2 : # linear
+            input = input.unsqueeze(-1,).unsqueeze(-1)
+
+        if training :
+            B, C, H, W = input.shape
+            y = input.transpose(0, 1).contiguous() # C x B x H x W
+            y = y.view(C, -1)  # C x (BHW)
+            sum = y.sum(-1)
+            mean = sum.div(B*H*W)
+            
+            square_sum = (y*y).sum(-1)
+            square_mean = square_sum.div(B*H*W)
+
+            var = square_mean - (mean * mean)
+            # var = y.var(-1, unbiased=False)
+            
+            var2 = y.var(-1, unbiased=True)
+          
+            with torch.no_grad():
+                running_mean.mul_(1-momentum).add_(
+                    mean*(momentum))
+                running_var.mul_(1-momentum).add_(
+                    var2*(momentum))
+        else :
+            mean = running_mean 
+            var  = running_var
+
+        rsqrt_var = torch.rsqrt(var.view(1, -1, 1, 1)+eps)
+        out =(input-mean.view(1,-1,1,1)) * rsqrt_var
+ 
+        ctx.weight = weight
+        ctx.bias   = bias
+        ctx.rsqrt  = rsqrt_var
+        # ctx.mean   = mean 
+        ctx.xhat   = out
+
+        if affine :
+            out = out * weight.view(1, -1, 1, 1)
+            out = out + bias.view(1, -1, 1, 1)
+
+
+            
+        return out 
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        weight    = ctx.weight 
+        bias      = ctx.bias 
+        rsqrt_var = ctx.rsqrt
+        xhat      = ctx.xhat
+
+        if grad_output.dim() == 2 : # linear
+            grad_output = grad_output.unsqueeze(-1,).unsqueeze(-1)
+
+        B, C, H, W = grad_output.shape
+        grad_output_r = grad_output.transpose(0, 1).contiguous().view(C, -1)  # C x (BHW)
+        xhat_r = xhat.transpose(0, 1).contiguous().view(C, -1)
+
+        # grad_bias /weight 
+        grad_bias = grad_output_r.sum(-1)
+        grad_weight = (grad_output_r * xhat_r).sum(-1)
+
+        # grad_input 
+
+        gamma_istd =  (weight.view(1, -1, 1, 1) * rsqrt_var)
+
+        grad_output_mean = grad_output_r.mean(-1).view(1, -1, 1, 1)
+        grad_output_delta = grad_output - grad_output_mean 
+        grad_input = gamma_istd * (grad_output_delta 
+                - xhat * grad_weight.view(1, -1, 1, 1).div(B*H*W))
+
+
+
+        # distd = weight.view(1, -1, 1, 1) *  
+        # print(grad_input.shape)
+
+        return grad_input, None, None, grad_weight, grad_bias,  None, None, None, None
